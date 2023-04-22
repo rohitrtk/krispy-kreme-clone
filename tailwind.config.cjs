180c549bf7f06638892d3e23f8567bd2197bce63
Rohit Kisto <rohit_rtk@hotmail.com>
2023-04-22 14:59:38 -0400

    Added about page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: false,
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"kk-red": "#CF2E2E",
+				"kk-green": "#006938"
+			},
+		}
 	},
 	plugins: [],
 }
